cc4618b4d9df39dae61183c6d43f8c539643384a
silkair <silkair1234@gmail.com>
2024-11-13 22:37:27 +0900

    test

--- src/main/java/com/example/techeer_partners_api_session/entity/test.java
+++
@@ -1,8 +0,0 @@
-package com.example.techeer_partners_api_session.entity;
-
-public class test {
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-}
---
+++ src/main/java/com/example/techeer_partners_api_session/test.java
@@ -0,0 +1,5 @@
+package com.example.techeer_partners_api_session;
+
+public class test {
+    //테스트 입니다
+}

4d4fd30c3906a9c236e4eb190dfa43aca3774fb4
silkair <silkair1234@gmail.com>
2024-11-21 04:16:53 +0900

    feat(service): TaskService 생성

---
+++ src/main/java/com/example/techeer_partners_api_session/service/TaskService.java
@@ -0,0 +1,19 @@
+package com.example.techeer_partners_api_session.service;
+
+import com.example.techeer_partners_api_session.dto.TaskRequestDto;
+import com.example.techeer_partners_api_session.entity.Task;
+import com.example.techeer_partners_api_session.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public void createTask(TaskRequestDto dto) {
+        Task task = new Task(dto.getTitle());
+    }
+}

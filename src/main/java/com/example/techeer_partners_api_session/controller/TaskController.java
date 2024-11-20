b2d7e7b50e3e2e38d2773524aec66d3c6ade6ff1
silkair <silkair1234@gmail.com>
2024-11-21 05:06:45 +0900

    feat(controller): TaskController 생성

---
+++ src/main/java/com/example/techeer_partners_api_session/controller/TaskController.java
@@ -0,0 +1,30 @@
+package com.example.techeer_partners_api_session.controller;
+
+import com.example.techeer_partners_api_session.dto.TaskRequestDto;
+import com.example.techeer_partners_api_session.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> createTask (@RequestBody TaskRequestDto dto) {
+        taskService.createTask(dto);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "할 일이 성공적으로 생성되었습니다.");
+        return ResponseEntity.status(201).body(response);
+    }
+}

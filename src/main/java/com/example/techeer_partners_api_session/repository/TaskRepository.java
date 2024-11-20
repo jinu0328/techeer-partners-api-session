bc5c175cac8642882e3f81028237103e9b47f87c
silkair <silkair1234@gmail.com>
2024-11-21 06:23:06 +0900

    feat(repository): 완료 상태에 따른 필터링

--- src/main/java/com/example/techeer_partners_api_session/repository/TaskRepository.java
+++ src/main/java/com/example/techeer_partners_api_session/repository/TaskRepository.java
@@ -4,6 +4,9 @@ import com.example.techeer_partners_api_session.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByIsDone(boolean isDone);
 }

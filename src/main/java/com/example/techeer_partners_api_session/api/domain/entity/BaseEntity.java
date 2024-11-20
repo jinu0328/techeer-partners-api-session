7f1423a3fbb2d1c55d267eb3d30c26e13a3ea836
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-20 13:38:37 +0900

    feat: BaseEntity 구현

---
+++ src/main/java/com/example/techeer_partners_api_session/api/domain/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.example.techeer_partners_api_session.api.domain.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}

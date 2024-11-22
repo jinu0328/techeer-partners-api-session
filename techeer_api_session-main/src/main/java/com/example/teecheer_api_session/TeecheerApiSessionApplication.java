94c24fa60756faf3ad324742f2f059f8dae3df85
DOHOON <kkdh1215@gmail.com>
2024-11-23 02:18:41 +0900

    Add files via upload
    upload

---
+++ techeer_api_session-main/src/main/java/com/example/teecheer_api_session/TeecheerApiSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.teecheer_api_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeecheerApiSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeecheerApiSessionApplication.class, args);
+	}
+
+}
---
+++ techeer_api_session-main/src/main/java/com/example/teecheer_api_session/exception/TaskNotFoundException.java
@@ -0,0 +1,36 @@
+package com.example.teecheer_api_session.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class TaskNotFoundException {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "error");
+        response.put("message", "유효성 검사 실패");
+        response.put("errors", errors);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "error");
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
